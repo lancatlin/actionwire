8b80daea0bf11c2d0d78e2d187c7ba5baa8f5c1c
Justin Lin <wancat@wancat.cc>
2025-09-26 19:56:19 +1000

    feat: create observable from synchan

--- actionwire/synchan.py
+++ actionwire/synchan.py
@@ -1,24 +1,53 @@
+from dataclasses import dataclass
+import reactivex
+from reactivex.abc.observer import ObserverBase
+from reactivex.observable import Observable
 import socketio
 
-sio = socketio.Client()
+
+@dataclass
+class SynchanState:
+    playing: bool
+    currentTime: float
+    duration: float
+    loop: bool
+    latency: float
 
 
-@sio.event
-def connect():
-    print("connection established")
+def create_socket(observer: ObserverBase[SynchanState], scheduler):
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        print("connection established")
+
+    @sio.event
+    def control(data):
+        print("message received with ", data)
+        nonce = data["nonce"]
+        observer.on_next(
+            SynchanState(
+                playing=data["playing"],
+                currentTime=data["currentTime"],
+                duration=data["duration"],
+                loop=data["loop"],
+                latency=data["latency"],
+            )
+        )
+        sio.emit("ping", nonce)
+
+    @sio.event
+    def disconnect():
+        print("disconnected from server")
+        observer.on_completed()
+
+    sio.connect("http://localhost:3000")
+    sio.wait()
 
 
-@sio.event
-def control(data):
-    print("message received with ", data)
-    nonce = data["nonce"]
-    sio.emit("ping", nonce)
+def create_synchan() -> Observable[SynchanState]:
+    return reactivex.create(create_socket)
 
 
-@sio.event
-def disconnect():
-    print("disconnected from server")
-
-
-sio.connect("http://localhost:3000")
-sio.wait()
+if __name__ == "__main__":
+    create_synchan().subscribe(print)
