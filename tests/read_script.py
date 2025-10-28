fe9b4cd733dd26fd0c31775465e6d6355b25a757
Justin Lin <wancat@wancat.cc>
2025-10-28 11:12:18 +1100

    feat: read timecode from csv

---
+++ tests/read_script.py
@@ -0,0 +1,23 @@
+import csv
+from typing import Dict, List
+
+
+def read_script(file: str) -> Dict[str, List[str]]:
+    with open(file, "r") as f:
+        reader = csv.DictReader(f, ["timecode", "keyword", "event", "action", "p", "w"])
+        reader.__next__()
+
+        result: Dict[str, List[str]] = {}
+        for row in reader:
+            print(row)
+            keyword, timecode = row["keyword"], row["timecode"]
+            if keyword in result:
+                result[keyword].append(timecode)
+            else:
+                result[keyword] = [timecode]
+
+        return result
+
+
+if __name__ == "__main__":
+    print(read_script("./data/script.csv"))
